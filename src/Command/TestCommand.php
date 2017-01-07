b6714d43b0c236fa72dad359419008798196b646
Hannes Forsgård <hannes.forsgard@fripost.org>
2017-01-07 18:55:37 +0100

    Add bootstrap to cli command, close #6

--- src/Command/TestCommand.php
+++ src/Command/TestCommand.php
@@ -17,18 +17,32 @@ class TestCommand extends Command
     protected function configure()
     {
         $this->setName('test')
-            ->setDescription('Test examples in readme files')
+            ->setDescription('Test examples in readme file')
             ->addArgument(
                 'filename',
                 InputArgument::OPTIONAL | InputArgument::IS_ARRAY,
                 'Name of file to test',
                 ['README.md']
             )
+            ->addOption(
+                'bootstrap',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'A "bootstrap" PHP file that is run before testing'
+            )
+            ->addOption(
+                'no-auto-bootstrap',
+                null,
+                InputOption::VALUE_NONE,
+                "Don't try to load a local composer autoloader when boostrap is not definied"
+            )
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->bootstrap($input, $output);
+
         $tester = new ReadmeTester;
         $exitStatus = 0;
 
@@ -50,5 +64,28 @@ class TestCommand extends Command
         }
 
         return $exitStatus;
+    }
+
+    private function bootstrap(InputInterface $input, OutputInterface $output)
+    {
+        if ($filename = $input->getOption('bootstrap')) {
+            if (!file_exists($filename) || !is_readable($filename)) {
+                throw new \RuntimeException("Unable to bootstrap $filename");
+            }
+
+            if ($output->isVerbose()) {
+                $output->writeln("Loading bootstrap <comment>$filename</comment>");
+            }
+
+            return require_once $filename;
+        }
+
+        if (!$input->getOption('no-auto-bootstrap') && is_readable('vendor/autoload.php')) {
+            if ($output->isVerbose()) {
+                $output->writeln("Loading bootstrap <comment>vendor/autoload.php</comment>");
+            }
+
+            return require_once 'vendor/autoload.php';
+        }
     }
 }
