3c8585aa5f7e2bfeb9117963bcd3c5da9d2de466
Hannes Forsgård <hannes.forsgard@fripost.org>
2017-12-13 10:00:27 +0100

    Skip calling constructor in TestCase

--- src/PHPUnit/ReadmeTestCase.php
+++ src/PHPUnit/ReadmeTestCase.php
@@ -6,18 +6,8 @@ namespace hanneskod\readmetester\PHPUnit;
 
 class ReadmeTestCase extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * @var AssertReadme
-     */
-    private $asserter;
-
-    public function __construct(AssertReadme $asserter = null)
-    {
-        $this->asserter = $asserter ?: new AssertReadme($this);
-    }
-
     public function assertReadme(string $filename)
     {
-        $this->asserter->assertReadme($filename);
+        (new AssertReadme($this))->assertReadme($filename);
     }
 }
