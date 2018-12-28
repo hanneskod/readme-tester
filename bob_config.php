424213d6b3f4db86c4bbc918fe1e42fd29302e6a
Hannes Forsgård <hannes.forsgard@fripost.org>
2018-12-28 11:16:29 +0100

    Drop phpeg as a dev dependency, close #32

--- bob_config.php
+++ bob_config.php
@@ -2,10 +2,10 @@
 
 namespace Bob\BuildConfig;
 
-task('default', ['test', 'examples', 'phpstan', 'sniff']);
+task('default', ['test', 'phpstan', 'sniff']);
 
 desc('Run unit and feature tests');
-task('test', ['phpunit', 'phpspec', 'behat']);
+task('test', ['phpunit', 'phpspec', 'behat', 'examples']);
 
 desc('Run phpunit tests');
 task('phpunit', ['src/Parser/Parser.php'], function() {
@@ -49,6 +49,17 @@ task('build_parser', ['src/Parser/Parser.php']);
 $parserFiles = fileList('*.peg')->in([__DIR__ . '/src/Parser']);
 
 fileTask('src/Parser/Parser.php', $parserFiles, function() {
-    sh('vendor/bin/phpeg generate src/Parser/Parser.peg', null, ['failOnError' => true]);
+    sh('phpeg generate src/Parser/Parser.peg', null, ['failOnError' => true]);
     println('Generated parser');
+});
+
+desc('Globally install development tools');
+task('install_dev_tools', function() {
+    sh('composer global require consolidation/cgr', null, ['failOnError' => true]);
+    sh('cgr scato/phpeg:^1.0', null, ['failOnError' => true]);
+    sh('cgr phpunit/phpunit:^7', null, ['failOnError' => true]);
+    sh('cgr phpspec/phpspec:^5.1', null, ['failOnError' => true]);
+    sh('cgr behat/behat:^3.3', null, ['failOnError' => true]);
+    sh('cgr phpstan/phpstan', null, ['failOnError' => true]);
+    sh('cgr squizlabs/php_codesniffer:^3', null, ['failOnError' => true]);
 });
